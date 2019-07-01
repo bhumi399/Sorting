2c463fd19ac101108efd79da6fc10f014c2c00ab
bhumi399 <bhumi399@gmail.com>
2019-07-01 16:55:28 -0500

    Add files via upload

---
+++ Sorting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+	public static void Main(String[] args)
+	{
+		int[] arr = new int[] {5,40,80,65,87,43,9 };
+
+		Array.Sort(arr);
+
+		//Array.Reverse(arr);
+
+		foreach (var i in arr)
+		{
+			Console.Write(i + " ");
+
+		}
+
+        Console.ReadLine();
+	}
+	
+}
